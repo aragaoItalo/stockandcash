b150f50c669c9a2966f17e7d5ac8a62ac1322e1c
noaru35 <vinicius.s.bastos55@gmail.com>
2024-11-10 22:33:37 -0300

    Criei arquivos para sidebar, e para o card de conteudo da dashboard, atualizei o ícone de faHome para faBox

---
+++ frontend/src/Components/Dashboard/cards/resumoEstoque.jsx
@@ -0,0 +1,19 @@
+// ResumoEstoqueCard.jsx
+
+import React from 'react';
+import './css/resumoEstoque.css'; // Importando o arquivo de estilos
+
+const ResumoEstoque = () => {
+  return (
+    <div className="resumo-estoque-card">
+      <h5>Resumo de Estoque</h5>
+      <div className="resumo-conteudo">
+        <p><strong>Total de Produtos:</strong> 150</p>
+        <p><strong>Itens em Falta:</strong> 5</p>
+        <p><strong>Última Atualização:</strong> 10/11/2024</p>
+      </div>
+    </div>
+  );
+};
+
+export default ResumoEstoque;
---
+++ frontend/src/Components/Sidebar/sidebar.jsx
@@ -0,0 +1,99 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { FaPlus, FaBox, FaListUl, FaClipboard, FaCog, } from 'react-icons/fa';
+import './sidebar.css';
+import AdicionarProduto from '../adicionarProduto/cadastroProdutos';
+
+const Sidebar = () => {
+  const [isPopupVisible, setIsPopupVisible] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const navigate = useNavigate();
+
+  // Função para abrir o modal
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  // Função para fechar o modal
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  // Função para exibir ou esconder o popup
+  const togglePopup = () => {
+    setIsPopupVisible(!isPopupVisible);
+  };
+
+  // Função de logout
+  const handleLogout = () => {
+    console.log('Logout');
+    navigate('/login');
+  };
+
+  // Função para navegar até a página de estoque
+  const handleGoToEstoque = () => {
+    navigate('/estoque'); // Redireciona para a página de estoque
+  };
+
+  const handleGoToDashboard = () => {
+    navigate('/dashboard')
+  }
+
+  return (
+    <div>
+      <div className="dashboard-sidebar">
+        <div className="dashboard-items">
+          <ul className='dash-list'>
+            <li>
+              <button  onClick={handleGoToDashboard} className='sidebar-btn'>
+                <img src="src/assets/logo.png" alt="Logo" className='sidebar-logo' />
+              </button>
+            </li>
+            <li>
+              <button className="sidebar-button" onClick={openModal}>
+                <FaPlus className="fa-plus-icon" /> {/* Ícone de Adicionar Produto */}
+              </button>
+            </li>
+            <li>
+              <button className='sidebar-button' onClick={handleGoToEstoque}>
+                <FaBox />
+              </button>
+            </li>
+            <li>
+              <button className='sidebar-button'>
+                <FaListUl />
+              </button>
+            </li>
+            <li>
+              <button className='sidebar-button'>
+                <FaClipboard />
+              </button>
+            </li>
+          </ul>
+        </div>
+        <div className="dashboard-footer">
+          <button className='sidebar-button' onClick={togglePopup}>
+            <FaCog className="fa-cog-icon" />
+          </button>
+          <div className={`popup ${isPopupVisible ? 'show' : 'hide'}`}>
+            <p onClick={handleLogout}>Sair da Conta</p>
+          </div>
+        </div>
+      </div>
+
+      {isModalOpen && (
+        <div className="modal-overlay">
+          <div className="modal-content">
+            <AdicionarProduto onClose={closeModal} />
+            <button className="close-modal-btn" onClick={closeModal}>
+              Fechar
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Sidebar;
